212e6cc208619b42ea103d21a8bf52064db5eef5
tobiecameron <tobie@here.net.au>
2025-06-10 13:00:38 +1200

    new build wiht Meta info

--- schemas/index.js
+++
@@ -1,6 +0,0 @@
-import siteSettings from "./siteSettings.js"
-import post from "./post.js"
-import homePage from "./homePage.js"
-import workItem from "./workItem.js"
-
-export const schemaTypes = [siteSettings, post, homePage, workItem]
